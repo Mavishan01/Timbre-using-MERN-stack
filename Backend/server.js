aaa22937b317918c7f8aa041871603f0db4d11be
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2024-08-12 11:13:25 +0530

    Backend

---
+++ Backend/server.js
@@ -0,0 +1,37 @@
+// This code sets up a web server using Express and connects it to a MongoDB database. It defines routes for handling workout-related requests and logs every request to the console.
+require("dotenv").config();
+const express = require("express");
+const app = express(); //Express app
+const mogoose = require("mongoose");
+
+// const workoutRouts = require("./routes/workouts");
+
+
+// Middleware
+//  This middleware converts incoming request bodies to JSON
+app.use(express.json());
+
+// Routes
+// app.use("/api/workouts", workoutRouts);
+
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+});
+
+// Connect to database
+mogoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+    // Listen for requests
+    app.listen(process.env.PORT, () => {
+        console.log("Connected to db & listening to port", process.env.PORT);
+    });
+    })
+    .catch((error) => {
+    console.log(error);
+    });
+
+
+
