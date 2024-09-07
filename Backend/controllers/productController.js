7023a987477db35e858f5a05546b715bf9f5b3c6
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2024-09-07 09:45:45 +0530

    Product controllers

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -43,7 +43,7 @@ const createProduct = async (req, res) => {
             category_id,
             price,
             quantity,
-            img_card:image,
+            img_card: image,
             color_id,
             ratings,
             brand_id,
@@ -64,14 +64,36 @@ const createProduct = async (req, res) => {
 
 const updateProduct = async (req, res) => {
     const productID = req.params.id;
-    const updateData = req.body;
+    const { title, description, category_id, price, quantity, color_id, brand_id, model_id } = req.body;
+
+    let updateData = {
+        title,
+        description,
+        category_id,
+        price,
+        quantity,
+        color_id,
+        brand_id,
+        model_id,
+    };
+
+    if (req.file) {
+        const image = req.file.filename;
+        updateData.img_card = image;
+    }
 
     try {
-        const updatedProduct = await Product.findByIdAndUpdate(
+        await Product.findByIdAndUpdate(
             productID,
             updateData,
             { new: true }
         );
+
+        const updatedProduct = await Product.findById(productID)
+            .populate("brand_id", "name")
+            .populate("model_id", "name")
+            .populate("color_id", "name")
+            .populate("category_id", "name");
 
         if (!updatedProduct) {
             console.log("Product not found");
@@ -84,6 +106,7 @@ const updateProduct = async (req, res) => {
             message: 'Product updated successfully',
             product: updatedProduct,
         });
+
 
     } catch (error) {
         console.error('Error updating product:', error);
@@ -111,6 +134,7 @@ const deleteProduct = async (req, res) => {
 const getNewArrivals = async (req, res) => {
     try {
         const products = await Product.find()
+            .populate('brand_id', 'name')
             .sort({ createdAt: -1 })
             .limit(6);
         if (!products) {
@@ -123,10 +147,58 @@ const getNewArrivals = async (req, res) => {
     }
 }
 
+const mostRatedProducts = async (req, res) => {
+    try {
+        const products = await Product.find().sort({ ratings: -1 }).limit(6);
+        if (!products) {
+            res.status(400).json({ message: 'Products not found', error });
+        }
+        return res.status(200).json({ message: "Success", products: products })
+    } catch (error) {
+        return res.status(500).json({ message: "Internal server error" })
+    }
+}
+
+const getProductsByCategory = async (req, res) => {
+    const id = req.params.id
+    try {
+        const category = Category.findById(id)
+        if (!category) {
+            return res.status(400).json({ message: 'Category not found' })
+        }
+        const products = await Product.find({ category_id: id })
+        if (!products) {
+            return res.status(400).json({ message: 'Products not found' })
+        }
+        return res.status(200).json({ message: "Success", products: products })
+    } catch (error) {
+        return res.status(500).json({ message: 'Internal Server error' })
+    }
+}
+
+const getProductsByBrands = async (req, res) => {
+    const { brands } = req.body
+    try {
+        if (!brands || brands.length === 0) {
+            return res.status(400).json({ message: 'Please select at least one brand' })
+        }
+        const products = await Product.find({ brand_id: { $in: brands } })
+        if (products.length === 0) {
+            return res.status(400).json({ message: 'No products found' })
+        }
+        return res.status(200).json({ message: "Success", products: products })
+    } catch (error) {
+        return res.status(500).json({ message: 'Internal Server error' })
+    }
+}
+
 module.exports = {
     getProducts,
     createProduct,
     updateProduct,
     deleteProduct,
-    getNewArrivals
+    getNewArrivals,
+    mostRatedProducts,
+    getProductsByCategory,
+    getProductsByBrands
 }
--- Backend/routes/productRoute.js
+++ Backend/routes/productRoute.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const { getProducts, createProduct, updateProduct, deleteProduct, getNewArrivals } = require("../controllers/productController");
+const { getProducts, createProduct, updateProduct, deleteProduct, getNewArrivals, mostRatedProducts, getProductsByCategory, getProductsByBrands } = require("../controllers/productController");
 const createUploadMiddleware = require("../middlewear/uploadMiddleware");
 
 
@@ -9,14 +9,17 @@ const uploadProductImage = createUploadMiddleware("products", "image");
 router.get("/", getProducts);
 
 // create Product
-router.post("/", uploadProductImage,createProduct);
+router.post("/", uploadProductImage, createProduct);
 
 // Update Product
-router.put("/update/:id", updateProduct);
+router.put("/update/:id",uploadProductImage, updateProduct);
 
 // Delete Product 
 router.delete("/delete/:id", deleteProduct);
 
 router.get("/new-arrivals", getNewArrivals)
+router.get("/most-rated", mostRatedProducts)
+router.get("/by-category/:id", getProductsByCategory)
+router.post('/by-brands/', getProductsByBrands)
 
 module.exports = router;
